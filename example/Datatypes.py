5f605349f745e6b20a64cb3ce4930cc769ebc215
amoomirr <amoomirr@gmail.com>
2024-06-26 20:47:50 +0530

    update

--- example/Datatypes.py
+++ example/Datatypes.py
@@ -36,7 +36,7 @@ print(X)
 X = 'AMIR'
 for i in X:
     print(X)
-
+    
 #2 Tuple
 #Tuple items are ordered, unchangeable, and allow duplicate values. Tuple items are indexed, the first item has index [0], the second item has index [1] etc.
 
--- example/QRcode.py
+++ example/QRcode.py
@@ -1,14 +1,15 @@
 #pip install pyqrcode
+#Preferred installer Program
 
 import pyqrcode #module package/Dependecies
 from pyqrcode import QRCode #it will extract QRcode from the pyqrcode package
   
 # String which represent the QR code 
-s = "https://www.google.com/"
+b = "https://www.instagram.com/_amir1282/"
   
 # Generate QR code 
-url = pyqrcode.create(s) #actual package which is having functions or methods
+a = pyqrcode.create(b) #actual package which is having functions or methods
   
 # Create and save the png file naming "myqr.png" 
-#Scalable Vector Graphics
-url.svg("mygoogle.svg", scale = 8)
+#Scalable Vector Graphics 
+a.svg("myinstagram.svg", scale = 4)
